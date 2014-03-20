cb2b1b988c0ae8ccb745ef1a807bb04b1dbe423c
Alexander Gamble <alexander.gamble@outlook.com>
2014-03-20 22:26:13 +0000

    initial commit

---
+++ flaxman/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for flaxman project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flaxman.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
