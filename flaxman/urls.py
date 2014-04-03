a2501fc3453c824229baa1ea9343770f484741fa
Alexander Gamble <alexander.gamble@outlook.com>
2014-04-03 13:28:32 +0300

    changes on the way to angular integration

--- flaxman/urls.py
+++ flaxman/urls.py
@@ -3,9 +3,8 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.conf import settings
-
-
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -15,7 +14,9 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^news/', include('news.urls')),
     url(r'^media/', include('media_browser.urls')),
+    url(r'^api/', include('api.urls')),
+
     url(r'^admin/', include(admin.site.urls)),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
