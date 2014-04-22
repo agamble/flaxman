0196b6ad697c88da25541432a6ce9b9d8568cfa3
ts309 <fu.yin.11@ucl.ac.uk>
2014-04-22 09:40:07 +0100

    Update factories.js
    hehehe

--- static/js/factories.js
+++ static/js/factories.js
@@ -1,8 +1,32 @@
+
+
 flaxman.factory('Media', ['$http', '$q',
     function MediaFactory($http, $q) {
 
-        var Media = {};
-        Media.home = []
+        var Media = {}; 
+        Media.home = [];
+	alert("mediafactory called");
+	Media.lel = function($scope) {
+		$q.all([Media.getFirst, Media.getAll]).then(function(values) {
+            Media.home.push(values[0].data.splice(0, Math.ceil(values[0].data.length / 3)));
+            Media.home.push(values[0].data.splice(0, Math.ceil(values[0].data.length / 2)));
+            Media.home.push(values[0].data);
+            Media.home[0] = Media.home[0].concat(values[1].data.splice(0, Math.ceil(values[1].data.length / 3)));
+            Media.home[1] = Media.home[1].concat(values[1].data.splice(0, Math.ceil(values[1].data.length / 2)));
+            Media.home[2] = Media.home[2].concat(values[1].data);
+	c = JSON.parse(JSON.stringify(Media.home));
+		for (var i = 0; i < 1; i++){
+			d[0][i] = c[0][i];
+			d[1][i] = c[1][i];
+			d[2][i] = c[2][i];
+		}
+		alert("data finally fetched!"); alert("d is now:"+d); console.log("c:"+c);console.log("d:"+d);
+		$scope.media=d;        //delayed the scope update to wait for the http request to return heheheheh...
+	}) 
+		//note that this block executes BEFORE the $q block above has returned, therefore c here is empty but above, c is Media.home returned. 
+		console.log("media home:", Media.home); 
+		alert("lel");
+	}
         Media.getSingle = function(id) {
             return $http.get('/api/media/' + id + '.json')
         }
@@ -13,24 +37,31 @@ flaxman.factory('Media', ['$http', '$q',
             })
         }
 
-        Media.getFirst = $http.get('/api/media/first.json')
+        Media.getFirst = $http.get('/api/media/first.json').then(function (results) {
+alert("http request returned!");
+return results;
+}); 
 
         Media.getAll = $http.get('/api/media.json')
 
-        $q.all([Media.getFirst, Media.getAll]).then(function(values) {
+     /*   $q.all([Media.getFirst, Media.getAll]).then(function(values) {
             Media.home.push(values[0].data.splice(0, Math.ceil(values[0].data.length / 3)));
             Media.home.push(values[0].data.splice(0, Math.ceil(values[0].data.length / 2)));
             Media.home.push(values[0].data);
             Media.home[0] = Media.home[0].concat(values[1].data.splice(0, Math.ceil(values[1].data.length / 3)));
             Media.home[1] = Media.home[1].concat(values[1].data.splice(0, Math.ceil(values[1].data.length / 2)));
             Media.home[2] = Media.home[2].concat(values[1].data);
+		alert("data finally fetched!");
         })
+	*/
+            
 
         Media.getVimeoURL = function(id, callback) {
             $http.get('http://vimeo.com/api/v2/video/' + id + '.json').success(function(data) {
                 callback(data[0].large_thumbnail)
             })
         }
+
         return Media;
 
     }
@@ -53,7 +84,7 @@ flaxman.factory('Playlist', ['$http', '$q',
             })
         }
 
-        return Playlists
+        return Playlists;
     }
 ])
 
