41e9bbc71056ee08d5b189451333c6fbcc95386a
Alexander Gamble <alexander.gamble@outlook.com>
2014-04-28 16:25:11 +0100

    fixed infinite scroll and made it a directive

--- static/js/factories.js
+++ static/js/factories.js
@@ -2,63 +2,61 @@
 flaxman.factory('Media', ['$http', '$q',
     function MediaFactory($http, $q) {
 
-
-
-counter = number_of_cards_in_each_row_to_load_at_start;
-
         var Media = {};
-        Media.home = [];
-Media.lel = function($scope) {
-$q.all([Media.getFirst, Media.getAll]).then(function(values) {
-            Media.home.push(values[0].data.splice(0, Math.ceil(values[0].data.length / 3)));
-            Media.home.push(values[0].data.splice(0, Math.ceil(values[0].data.length / 2)));
-            Media.home.push(values[0].data);
-            Media.home[0] = Media.home[0].concat(values[1].data.splice(0, Math.ceil(values[1].data.length / 3)));
-            Media.home[1] = Media.home[1].concat(values[1].data.splice(0, Math.ceil(values[1].data.length / 2)));
-            Media.home[2] = Media.home[2].concat(values[1].data);
-c = Media.home;
-for (var i = 0; i < number_of_cards_in_each_row_to_load_at_start; i++){
-d[0][i] = c[0][i];
-d[1][i] = c[1][i];
-d[2][i] = c[2][i];
-}
-$scope.media=d; //delayed the scope update to wait for the http request to return heheheheh...
-})
-//note that this block executes BEFORE the $q block above has returned, therefore c here is empty but above, c is Media.home returned.
-console.log("media home:", Media.home);
-}
+
+        var card_incrementer = 10;
+        Media.getFirst = $http.get('/api/media/first.json');
+
+        Media.getAll = $http.get('/api/media.json');
+
+        $q.all([Media.getFirst, Media.getAll]).then(function(values) {
+            var hidden_cards = [];
+            hidden_cards.push(values[0].data.splice(0, Math.ceil(values[0].data.length / 3)));
+            hidden_cards.push(values[0].data.splice(0, Math.ceil(values[0].data.length / 2)));
+            hidden_cards.push(values[0].data);
+            hidden_cards[0] = hidden_cards[0].concat(values[1].data.splice(0, Math.ceil(values[1].data.length / 3)));
+            hidden_cards[1] = hidden_cards[1].concat(values[1].data.splice(0, Math.ceil(values[1].data.length / 2)));
+            hidden_cards[2] = hidden_cards[2].concat(values[1].data);
+            Media.hiddenCards = hidden_cards;
+            initialiseViewableCards();
+        })
+
+        Media.viewableCards = [
+            [],
+            [],
+            []
+        ]
+
+        var initialiseViewableCards = function() {
+            Media.viewableCards[0] = Media.hiddenCards[0].slice(0, 10);
+            Media.viewableCards[1] = Media.hiddenCards[1].slice(0, 10);
+            Media.viewableCards[2] = Media.hiddenCards[2].slice(0, 10);
+        }
+
+        Media.addMoreMedia = function() {
+            card_incrementer += 10;
+            Media.viewableCards[0].concat(Media.hiddenCards[0].slice(card_incrementer - 10, card_incrementer));
+            Media.viewableCards[1].concat(Media.hiddenCards[1].slice(card_incrementer - 10, card_incrementer));
+            Media.viewableCards[2].concat(Media.hiddenCards[2].slice(card_incrementer - 10, card_incrementer));
+        }
+
         Media.getSingle = function(id) {
             return $http.get('/api/media/' + id + '.json')
         }
-
         Media.getRelated = function(id, callback) {
             $http.get('/api/media/related/' + id + '.json').success(function(data, status) {
                 callback(data)
             })
         }
 
-        Media.getFirst = $http.get('/api/media/first.json').then(function (results) {
-return results;
-});
-
-        Media.getAll = $http.get('/api/media.json')
-
-     /* $q.all([Media.getFirst, Media.getAll]).then(function(values) {
-Media.home.push(values[0].data.splice(0, Math.ceil(values[0].data.length / 3)));
-Media.home.push(values[0].data.splice(0, Math.ceil(values[0].data.length / 2)));
-Media.home.push(values[0].data);
-Media.home[0] = Media.home[0].concat(values[1].data.splice(0, Math.ceil(values[1].data.length / 3)));
-Media.home[1] = Media.home[1].concat(values[1].data.splice(0, Math.ceil(values[1].data.length / 2)));
-Media.home[2] = Media.home[2].concat(values[1].data);
-})
-*/
-            
 
         Media.getVimeoURL = function(id, callback) {
             $http.get('http://vimeo.com/api/v2/video/' + id + '.json').success(function(data) {
                 callback(data[0].large_thumbnail)
             })
         }
+
+
 
         return Media;
 
