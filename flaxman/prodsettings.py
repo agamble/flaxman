82d049f28f73d740ec900151b9f60d72852ee285
Alexander Gamble <alexander.gamble@outlook.com>
2014-04-29 17:17:58 +0100

    added some prod settings

---
+++ flaxman/prodsettings.py
@@ -0,0 +1,111 @@
+"""
+Django settings for flaxman project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ')0gmymau56aht69gt01ll*+!5g9sgg_3$i+4n-r^0sfk9l%=^n'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+TEMPLATE_DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'flaxman',
+    'media_browser',
+    'news',
+    'playlist',
+    'api',
+    'tinymce',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'flaxman.urls'
+
+WSGI_APPLICATION = 'flaxman.wsgi.application'
+
+TEMPLATE_DIRS = (
+    'django.template.loaders.app_directories',
+)
+
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+)
+
+STATIC_URL = '/static/'
+
+MEDIA_URL = '/upload/'
+
+MEDIA_ROOT = BASE_DIR + '/media/'
+# STATIC_ROOT = '/Users/alexander/workspace/flaxman/static/'
+
+STATICFILES_DIRS = ( 
+      BASE_DIR + '/static/',  
+)
+
+STATIC_ROOT = BASE_DIR + '/static-collected/'
