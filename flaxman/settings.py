dc821f012908cd4bb7ae9a03ba5c6272efbc2cb9
Alexander Gamble <alexander.gamble@outlook.com>
2014-05-05 17:14:33 +0100

    reverted change done to authentication

--- flaxman/settings.py
+++ flaxman/settings.py
@@ -70,7 +70,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'flaxman',
-        'USER': 'postgres',
+        'USER': 'flaxman',
         'PASSWORD': 'hermione',
         'HOST': 'localhost',
         'PORT': '5432',
